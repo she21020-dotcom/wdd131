fd7ae7cda9654b60afaa397f2ed642dc666a1b8f
benshelley56-sudo <benshelley56@gmail.com>
2026-09-18 14:14:24 -0600

    week01 ponder

---
+++ week01Ponder.js
@@ -0,0 +1,21 @@
+//run program command: node sandbox/week01.js
+
+const PI = 3.14;
+let radius = 3;
+
+
+
+const one = 1;
+const two = '2';
+                    
+
+let student = "John";
+let course = "CSE131"; //global scope
+if (true) {
+    
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
